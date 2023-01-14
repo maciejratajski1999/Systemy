f86e04ebd41cd742da2bb796f58fc31f9d7bc877
Maciej Ratajski <maciejratajski1999@gmail.com>
2023-01-14 19:53:13 +0100

    fixed no __init__ function bug

--- queue.py
+++ queue.py
@@ -1,6 +1,6 @@
 class Node:
 
-    def __int__(self, element=None):
+    def __init__(self, element=None):
         self.head = element
         self.tail = None
 
@@ -10,10 +10,15 @@ class Node:
         else:
             self.tail.enqueue(node)
 
+    def __str__(self):
+        if self.head is None:
+            return "end node"
+        else:
+            return str(self.head) + " --> " + str(self.tail)
 
 class Queue:
 
-    def __int__(self):
+    def __init__(self):
         self.front = None
 
     def enqueue(self, node):
@@ -27,3 +32,8 @@ class Queue:
         self.front = self.front.tail
         return front
 
+    def __repr__(self):
+        if self.front is None:
+            return "empty queue"
+        else:
+            return str(self.front)
