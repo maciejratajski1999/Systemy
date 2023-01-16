94fe353b55fb409bae3dfea1b37ff1434d373062
Maciej Ratajski <maciejratajski1999@gmail.com>
2023-01-16 21:27:07 +0100

    draw a plot

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, seed
 from datetime import datetime
 import csv
 
@@ -18,10 +18,11 @@ def generate_processes(n, max_arrival_time, max_execution_time):
                               f"process{i}") for i in range(n)]
     return processes
 
-def save_to_csv(n, max_arrival_time=100, max_execution_time=10):
+def save_to_csv(n, max_arrival_time=100, max_execution_time=10, custom_seed=256435):
     processes = generate_processes(n, max_arrival_time, max_execution_time)
-    iso_time = iso_time_format()
-    csv_file_name = f"processes_x{n}_{iso_time}.csv"
+    # iso_time = iso_time_format()
+    seed(custom_seed)
+    csv_file_name = f"data\\processes_x{n}_{custom_seed}"
     with open(csv_file_name, 'w') as csv_file:
         csv_file.write('name,arrive_time,execution_time')
         for process in processes:
@@ -42,11 +43,18 @@ def csv_to_list(csv_file_name):
     return processes
 
 def save_results_to_csv(finished_processes, name, prefix):
-    csv_file_name = f"{prefix}_{name}.csv"
+    name = name[5:]
+    csv_file_name = f"{prefix}\\{name}"
     with open(csv_file_name, 'w') as csv_file:
-        csv_file.write('exit_time,name,arrive_time,execution_time')
+        csv_file.write('name,arrive_time,execution_time,exit_time')
         for exit_time, process in finished_processes.items():
             csv_file.write('\n')
-            values = [str(exit_time)] + [str(v) for v in process.values()]
+            values = [str(v) for v in process.values()] + [str(exit_time)]
             csv_file.write(','.join(values))
     return csv_file_name
+
+def average_wait_time(finished_processes):
+
+    wait_times = [process['exit_time'] - process['arrive_time'] - process['execution_time']
+                  for process in finished_processes]
+    return sum(wait_times) / len(wait_times)
---
+++ main_processes.py
@@ -0,0 +1,39 @@
+from data_utils import save_to_csv, csv_to_list, save_results_to_csv, average_wait_time
+from FCFS import simulate_FCFS
+from RoundRobin import simulate_RoundRobin
+from sys import setrecursionlimit
+from matplotlib import pyplot as plt
+
+
+if __name__ == '__main__':
+    setrecursionlimit(2000)
+    number_of_processes = [10] + [50*i for i in range(1, 6)] + [100*i for i in range(3, 11)]
+    max_arrival_time = 500
+    max_execution_time = 10
+    data_csv = [save_to_csv(n, max_arrival_time, max_execution_time) for n in number_of_processes]
+    # csv_file_name = "processes_x20_2023-01-16T14_00_54.csv"
+    # data_csv = ["data\\processes_x10_2023-01-16T14_28_55.csv",
+    #             "data\\processes_x100_2023-01-16T14_28_55.csv",
+    #             "data\\processes_x1000_2023-01-16T14_28_55.csv"]
+    fcfs_average_wait_times = []
+    rr_average_wait_times = []
+    for csv_file_name in data_csv:
+        processes = csv_to_list(csv_file_name)
+
+        FCFS_served_processes = simulate_FCFS(processes, max_arrival_time)
+        fcfs_csv_file_name = save_results_to_csv(FCFS_served_processes, csv_file_name, "fcfs")
+        # print(f"Average wait time for FCFS for n={len(FCFS_served_processes)}:")
+        fcfs_results_as_list = csv_to_list(fcfs_csv_file_name)
+        fcfs_average_wait_times.append(average_wait_time(fcfs_results_as_list))
+
+
+        quantum = 5
+        RoundRobin_served_processes = simulate_RoundRobin(processes, quantum, max_arrival_time)
+        rr_csv_file_name = save_results_to_csv(RoundRobin_served_processes, csv_file_name, "rr")
+        # print(f"Average wait time for RoundRobin for n={len(RoundRobin_served_processes)}:")
+        rr_results_as_list = csv_to_list(rr_csv_file_name)
+        rr_average_wait_times.append(average_wait_time(rr_results_as_list))
+    plt.plot(number_of_processes, fcfs_average_wait_times)
+    plt.plot(number_of_processes, rr_average_wait_times)
+    plt.legend(["FCFS", "RoundRobin"])
+    plt.show()
