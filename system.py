df48c92c808001f147e4af6ec46f4a9d1e488ce8
Maciej Ratajski <maciejratajski1999@gmail.com>
2023-01-16 12:38:09 +0100

    separated system listening and serving processes logic from FCFS algorithm

---
+++ system.py
@@ -0,0 +1,25 @@
+class System:
+
+    def listen(self, timer, processes, processes_queue):
+        for process in processes:
+            if process['arrive_time'] == timer:
+                processes_queue.enqueue(process)
+                print(f"proessor clock: {timer}, enqueued process {process['name']}")
+
+    def __serve_next(self, timer, current_process, processes_queue):
+        current_process['begin_time'] = timer
+        current_process['process'] = processes_queue.dequeue()
+        if current_process['process'] is not None:
+            print(f"processor clock: {timer}, started serving process {current_process['process']['name']}")
+        return current_process
+
+    def serve(self, timer, current_process, processes_queue, served_processes):
+        if current_process['process'] is None:
+            current_process = self.__serve_next(timer, current_process, processes_queue)
+        else:
+            if current_process['begin_time'] + current_process['process']['execution_time'] <= timer:
+                served_processes[timer] = current_process['process']
+                current_process = self.__serve_next(timer, current_process, processes_queue)
+            else:
+                pass
+        return current_process
