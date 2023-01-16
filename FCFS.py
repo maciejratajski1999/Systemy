d1e6ad3666c1ddb5f8e2ab18529482f85f3b9a22
Maciej Ratajski <maciejratajski1999@gmail.com>
2023-01-16 12:53:11 +0100

    fixed not finishing last process

--- FCFS.py
+++ FCFS.py
@@ -7,8 +7,8 @@ def simulate(processes):
     timer = 0
     current_process = {'begin_time' : timer, 'process' : None}
     served_processes = {}
-    while len(processes_queue) != 0 or timer < 100:
+    while len(processes_queue) != 0 or timer < 100 or current_process['process'] is not None:
         system.listen(timer, processes, processes_queue)
-        system.serve(timer, current_process, processes_queue, served_processes)
+        current_process = system.serve(timer, current_process, processes_queue, served_processes)
         timer += 1
     return served_processes
