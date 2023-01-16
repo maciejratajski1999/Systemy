a39693d72aa02648e0f942481328b1a053043b0a
Maciej Ratajski <maciejratajski1999@gmail.com>
2023-01-16 23:40:39 +0100

    looking bad

--- main_pages.py
+++ main_pages.py
@@ -2,11 +2,12 @@ from pages_data_utils import generate_to_csv, csv_to_list
 from sys import setrecursionlimit
 from FIFO import simulate_FIFO
 from MFU import simulate_MFU
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    setrecursionlimit(2000)
-    n_of_pages = 16
-    n_of_page_loads = [10] + [50 * i for i in range(1, 6)] + [100 * i for i in range(3, 11)]
+    setrecursionlimit(4000)
+    n_of_pages = 32
+    n_of_page_loads = [100] + [200 * i for i in range(1, 11)]
     memory_size = 10
     fifo_average_times_loaded = []
     mfu_average_times_loaded = []
@@ -21,6 +22,20 @@ if __name__ == '__main__':
         mfu_times_loaded = simulate_MFU(pages_to_load, memory_size)
         mfu_times_loaded = [value for value in mfu_times_loaded.values()]
         mfu_average = sum(mfu_times_loaded) / len(mfu_times_loaded)
-        mfu_average_times_loaded.append(fifo_average)
-    print(fifo_average_times_loaded)
-    print(mfu_average_times_loaded)
+        mfu_average_times_loaded.append(mfu_average)
+    plt.plot(n_of_page_loads, fifo_average_times_loaded)
+    plt.plot(n_of_page_loads, mfu_average_times_loaded)
+    plt.legend(["FIFO", "MFU"])
+    plt.xlabel("n of pages loaded")
+    plt.ylabel("average times a single page was loaded into memory")
+    plt.savefig("times loaded comparison")
+    plt.close()
+
+    ratios = [mfu_average_times_loaded[i] / fifo_average_times_loaded[i]
+              for i in range(len(n_of_page_loads))]
+
+    plt.plot(n_of_page_loads, ratios)
+    plt.title(f"ratio of average times loaded for memory size={memory_size}")
+    plt.xlabel("n of pages loaded")
+    plt.ylabel("ratio MFU/FIFO")
+    plt.savefig("times loaded ratio")
