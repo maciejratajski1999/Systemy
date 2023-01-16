a191b6e496b08235e641c645c4d0f83a6214cae4
Maciej Ratajski <maciejratajski1999@gmail.com>
2023-01-16 22:52:33 +0100

    counting average times loaded per page

--- main_pages.py
+++ main_pages.py
@@ -7,8 +7,12 @@ if __name__ == '__main__':
     n_of_pages = 16
     n_of_page_loads = [10] + [50 * i for i in range(1, 6)] + [100 * i for i in range(3, 11)]
     memory_size = 10
+    fifo_average_times_loaded = []
     for n in n_of_page_loads:
         pages_file_name = generate_to_csv(n, n_of_pages)
         pages_to_load = csv_to_list(pages_file_name)
         times_loaded = simulate_FIFO(pages_to_load, memory_size)
-        print(times_loaded)
+        fifo_times_loaded = [value for value in times_loaded.values()]
+        fifo_average = sum(fifo_times_loaded) / len(fifo_times_loaded)
+        fifo_average_times_loaded.append(fifo_average)
+        print(fifo_average_times_loaded)
